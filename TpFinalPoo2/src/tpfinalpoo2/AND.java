d7e07756f3b5137cdf40e427576c5ca46d5444ea
Sanabria Cristian <crissanabria1995@gmail.com>
2025-10-25 14:30:17 -0300

    Se agrega la clase AND relacionada a la busqueda de viajes y su test

---
+++ TpFinalPoo2/src/tpfinalpoo2/AND.java
@@ -0,0 +1,27 @@
+package tpfinalpoo2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AND implements Filtro{
+	
+	private List<Filtro> filtros = new ArrayList<>();
+	
+    public void agregarFiltro(Filtro filtro) {
+        this.filtros.add(filtro);
+    }
+
+    public List<Viaje> filtrar(List<Viaje> viajes) {
+        List<Viaje> resultado = viajes;
+        for (Filtro filtro : filtros) {
+            resultado = filtro.filtrar(resultado);
+        }
+        return resultado;
+    }
+    
+    public List<Filtro> getFiltros(){
+    	return this.filtros;
+    }
+    
+}
+
---
+++ TpFinalPoo2/test/tpfinalpoo2/ANDTest.java
@@ -0,0 +1,59 @@
+package tpfinalpoo2;
+
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ANDTest {
+	
+	private AND filtroAND;
+	private FiltroFechaLlegada filtroFechaLlegada;
+	private FiltroFechaSalida filtroFechaSalida;
+	private FiltroPuertoDestino filtroPuertoDestino;
+	private Naviera naviera;
+	private Viaje viaje;
+	private Viaje otroViaje;
+	
+	@BeforeEach
+    public void setUp() {
+		filtroFechaLlegada = mock(FiltroFechaLlegada.class);
+		filtroFechaSalida = mock(FiltroFechaSalida.class);	
+		filtroPuertoDestino = mock(FiltroPuertoDestino.class);
+		naviera = mock(Naviera.class);
+		filtroAND = new AND();
+		viaje = mock(Viaje.class);
+		otroViaje = mock(Viaje.class);
+    }
+	
+	@Test
+	void testSePuedeAgregarUnFiltro() {
+		filtroAND.agregarFiltro(filtroFechaLlegada);
+		Assertions.assertTrue(filtroAND.getFiltros().contains(filtroFechaLlegada));
+	}
+	
+	@Test
+    void testSeAplicanLosFiltrosDeBusqueda() {
+
+		List<Viaje> listaViajes = Arrays.asList(viaje, otroViaje);
+		List<Viaje> listaFiltrada = Arrays.asList(viaje);
+		
+		when(naviera.getViajes()).thenReturn(listaViajes);
+		when(filtroFechaSalida.filtrar(listaViajes)).thenReturn(listaFiltrada);
+	    when(filtroPuertoDestino.filtrar(listaFiltrada)).thenReturn(listaFiltrada);
+		
+	    filtroAND.agregarFiltro(filtroFechaSalida);
+	    filtroAND.agregarFiltro(filtroPuertoDestino);
+	    
+		List<Viaje> resultado = filtroAND.filtrar(naviera.getViajes());
+		 
+		verify(filtroFechaSalida).filtrar(listaViajes);
+		verify(filtroPuertoDestino).filtrar(listaFiltrada);
+		Assertions.assertEquals(resultado, listaFiltrada);
+		Assertions.assertFalse(resultado.contains(otroViaje));
+	}
+}
