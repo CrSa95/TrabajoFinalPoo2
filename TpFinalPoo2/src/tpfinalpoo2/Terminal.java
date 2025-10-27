d0de16556d29e8238b75638ccae05757b9760d30
Sanabria Cristian <crissanabria1995@gmail.com>
2025-10-27 00:13:41 -0300

    Se agrega Terminal con comportamiento basico para que funcionen los test

---
+++ TpFinalPoo2/src/tpfinalpoo2/Terminal.java
@@ -0,0 +1,14 @@
+package tpfinalpoo2;
+
+public class Terminal {
+
+	private String nombre;
+	
+	public Terminal(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+}
