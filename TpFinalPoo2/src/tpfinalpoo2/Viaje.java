249895748b7859ae8d073a244906676db599dae8
Sanabria Cristian <crissanabria1995@gmail.com>
2025-10-27 00:12:53 -0300

    Se agrega Viaje con comportamiento basico para que funcionen los test

---
+++ TpFinalPoo2/src/tpfinalpoo2/Viaje.java
@@ -0,0 +1,26 @@
+package tpfinalpoo2;
+
+import java.time.LocalDate;
+
+public class Viaje {
+	
+	private LocalDate fechaLlegada;
+	private LocalDate fechaSalida;
+	private OtraTerminal terminalDestino;
+	
+	public LocalDate getFechaLlegada() {
+		// TODO Auto-generated method stub
+		return this.fechaLlegada;
+	}
+
+	public LocalDate getFechaSalida() {
+		// TODO Auto-generated method stub
+		return this.fechaSalida;
+	}
+
+	public OtraTerminal getTerminalDestino() {
+		// TODO Auto-generated method stub
+		return this.terminalDestino;
+	}
+
+}
