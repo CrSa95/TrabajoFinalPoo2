75763d480383a31e190291057c3f44be6b3bfc8a
Sanabria Cristian <crissanabria1995@gmail.com>
2025-10-28 01:57:35 -0300

    Mas metodos en Terminal: agregarNaviera, setEstrategia y buscarMejorCirc

--- TpFinalPoo2/src/tpfinalpoo2/Terminal.java
+++ TpFinalPoo2/src/tpfinalpoo2/Terminal.java
@@ -1,8 +1,13 @@
 package tpfinalpoo2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Terminal {
 
 	private String nombre;
+	private IStrategyBusquedaCircuito estrategiaBusquedaMejorCircuito;
+	private List<Naviera> navieras = new ArrayList<>();
 	
 	public Terminal(String nombre) {
 		this.nombre = nombre;
@@ -10,5 +15,31 @@ public class Terminal {
 	
 	public String getNombre() {
 		return this.nombre;
+	}
+	
+	public void agregarNaviera(Naviera naviera) {
+		this.getNavieras().add(naviera);
+	}
+	
+	public List<Naviera> getNavieras(){
+		return this.navieras;
+	}
+	
+	public void setEstrategiaDeBusqueda(IStrategyBusquedaCircuito strategyCircuitos) {
+		this.estrategiaBusquedaMejorCircuito = strategyCircuitos;
+	}
+	
+	public IStrategyBusquedaCircuito getEstrategiaDeBusqueda() {
+		return this.estrategiaBusquedaMejorCircuito;
+	}
+	
+	public Circuito buscarMejorCircuitoParaLLegarA(Terminal terminalDestino) {
+		
+		List<Circuito> todosLosCircuitos = this.getNavieras().stream()
+															 .map(Naviera::getCircuitos)
+															 .flatMap(List::stream)
+															 .toList(); 
+		
+		return this.getEstrategiaDeBusqueda().seleccionarMejor(todosLosCircuitos, this, terminalDestino);
 	}
 }
