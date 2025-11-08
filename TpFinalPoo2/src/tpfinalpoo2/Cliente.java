150576b33c4096eb007f89669957428241f36e5a
Sanabria Cristian <crissanabria1995@gmail.com>
2025-11-08 19:02:47 -0300

    Revert "Merge pull request #13 from CrSa95/featureOrden"
    This reverts commit d4fb92abf8f75a8b6fc83333402f32b57715e38b, reversing
    changes made to dd7792a9a3959a7b47ed064d6d7b7e84ba558dce.

--- TpFinalPoo2/src/tpfinalpoo2/Cliente.java
+++ TpFinalPoo2/src/tpfinalpoo2/Cliente.java
@@ -1,31 +1,14 @@
 package tpfinalpoo2;
 
 public class Cliente {
-	
-	private String nombre;
-	private String dni;
-  private String correo;
-	
-	public Cliente(String nombre, String dni, String correo) {
-		this.nombre = nombre;
-		this.dni = dni;
-    this.correo = correo;
+	private String correo;
+
+	public Cliente(String correo) {
+		this.correo = correo;
 	}
-	
-	public String nombre() {
-		return this.nombre;
-	}
-	
-	public String dni() {
-		return this.dni;
-	} 
 
 	public String correo() {
 		return this.correo;
-	}
-
-	public Cliente() {
-		this.correo = correo; // TO DO: Mover lo que use este constructor al otro
 	}
 
 	public void notificar() {
--- TpFinalPoo2/src/tpfinalpoo2/Orden.java
+++ TpFinalPoo2/src/tpfinalpoo2/Orden.java
@@ -1,89 +1,64 @@
 package tpfinalpoo2;
 
 import java.time.LocalDateTime;
-import java.util.Set;
 
 public class Orden {
-	
-
-	private LocalDateTime fecha_llegada;
-	private LocalDateTime fecha_salida;
-	private Terminal origen;
-	private Terminal destino;
-	private Viaje viaje_seleccionado;
-	private Set<Servicio> servicios_contratados;
 	private Container carga;
-	private Camion camion;
 	private Chofer chofer;
-	private Cliente dueno;
-	
-	public Orden(LocalDateTime fecha_llegada, LocalDateTime fecha_salida, Terminal origen, Terminal destino,
-			Viaje viaje_seleccionado, Set<Servicio> servicios_contratados, Container carga, Camion camion,
-			Chofer chofer, Cliente dueno) {
-		this.fecha_llegada = fecha_llegada;
-		this.fecha_salida = fecha_salida;
-		this.origen = origen;
-		this.destino = destino;
-		this.viaje_seleccionado = viaje_seleccionado;
-		this.servicios_contratados = servicios_contratados;
-		this.carga = carga;
+	private Camion camion;
+	private Cliente cliente;
+
+	public Orden(Container container, Camion camion, Chofer chofer, Cliente cliente) {
+		this.carga = container;
 		this.camion = camion;
 		this.chofer = chofer;
-		this.dueno = dueno;
-	}
-	
-	
-	public boolean verificar(Camion camionAVerificar, Chofer choferAVerificar, Container cargaAVerificar) {
-		return this.verificarCamion(camionAVerificar) && this.verificarChofer(choferAVerificar) && this.verificarCarga(cargaAVerificar);
-	}
-	
-	public double costoEnServicios() {
-		return this.servicios_contratados
-		        .stream()
-		        .mapToDouble(servicio -> servicio.costo(this.carga))
-		        .sum();
-	}
-	
-	public LocalDateTime fechaSalida() {
-		return this.fecha_salida;
-	}
-	
-	public LocalDateTime fechaLlegada() {
-		return this.fecha_llegada;
-	}
-	
-	public Viaje viaje() {
-    return this.viaje_seleccionado.equals(viaje);
-	}
-	
-	private boolean verificarCamion(Camion camionAVerificar) {
-	    return this.camion.patente().equals(camionAVerificar.patente());
-	}
-
-	private boolean verificarChofer(Chofer choferAVerificar) {
-	    return this.chofer.dni().equals(choferAVerificar.dni());
-	}
-
-	private boolean verificarCarga(Container cargaAVerificar) {
-		return true; // TO DO: Forma de identificar container
+		this.cliente = cliente;
 	}
 
 	public Container carga() {
 		return this.carga;
 	}
 
+	public LocalDateTime fechaRetiro() {
+		return null;
+	}
+
+	public LocalDateTime fechaIngreso() {
+		return null;
+	}
+
 	public boolean perteneceAlContainer(Container container) {
 		return this.carga.isEqual(container);
 	}
 
+	public void verificar(Container container, Camion camion, Chofer chofer) {
+		if (this.carga.isEqual(container)) {
+			this.verificarCamion(camion);
+			this.verificarChofer(chofer);
+		}
+
+	}
+
+	private void verificarChofer(Chofer chofer) {
+		if (!this.chofer.dni().equals(chofer.dni())) {
+			throw new RuntimeException();
+		}
+	}
+
+	private void verificarCamion(Camion camion) {
+		if (!this.camion.patente().equals(camion.patente())) {
+			throw new RuntimeException();
+		}
+	}
+
 	public void notificarLlegada(Buque buque) {
 		if (this.mismoViaje(buque.viaje())) {
-			this.dueno.notificar();
+			this.cliente.notificar();
 		}
 	}
 
 	private boolean mismoViaje(Viaje viaje) {
-		return this.viaje_seleccionado.equals(viaje);
+		return viaje.equals(viaje);
 	}
 
 }
