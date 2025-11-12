36f247e208060a6916d24237b975aed1832284b9
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2025-11-12 07:52:40 -0300

    se quito metodo vacio working

--- TpFinalPoo2/src/tpfinalpoo2/Working.java
+++ TpFinalPoo2/src/tpfinalpoo2/Working.java
@@ -6,11 +6,4 @@ public class Working extends EstadoGPS {
 	public void permitirSalida(Buque buque) {
 		buque.cambiarEstado(new Departing());
 	}
-
-	@Override
-	public void avanzar(Buque buque) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
