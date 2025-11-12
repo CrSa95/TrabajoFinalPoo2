59c08a1ad487b1c5b6bc629b94839b9a957f1cac
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2025-11-12 04:25:23 -0300

    Ahora el tests de Outbound verifica que outbound avisa su partida

--- TpFinalPoo2/test/tpfinalpoo2/OutboundTest.java
+++ TpFinalPoo2/test/tpfinalpoo2/OutboundTest.java
@@ -1,6 +1,6 @@
 package tpfinalpoo2;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Assertions;
@@ -9,22 +9,22 @@ import org.junit.jupiter.api.Test;
 
 public class OutboundTest {
 	
-	private Outbound estadoOutbound;
-	private Buque buqueMock;
-
+	Outbound suject;
+	Buque buque;
+	Terminal terminal;
+	
 	@BeforeEach
 	public void setUp() {
-		estadoOutbound = new Outbound();
-		buqueMock = mock(Buque.class);
+		suject = new Outbound();
+		buque = mock(Buque.class);
 	}
 
-	@Test
-	void testElEstadoInboundNoPuedeCambiarElEstadoAArrivedSiLaDistanciaADestinoNoEsLaCorrecta() {
-		
-		when(buqueMock.distanciaHaciaDestino()).thenReturn(60d);
-		EstadoGPS cambioDeEstado = estadoOutbound.actualizarGPS(buqueMock);
-		Assertions.assertEquals(estadoOutbound, cambioDeEstado);
-		
+	
+	@Test 
+	void seNotificaLaPartidaDelBuque() {
+		when(buque.terminaOrigen()).thenReturn(terminal);
+		suject.avisarPartida(buque);
+		verify(buque).avisarPartida();
 	}
 	
 }
