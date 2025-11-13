8ae7c0d93c3f2b4ff886c87900b258683b546ddb
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2025-11-13 13:33:18 -0300

    Se agrego el metodo facturar a la clase Orden
    - Se agregaron casos de prueba
    	1. para verificar que Orden factura a consignee si es una orden de
    importacion
    	2. para verificar que Orden factura a Shipper si es una orden de
    exportacion

---
+++ TpFinalPoo2/src/tpfinalpoo2/FacturaShipper.java
@@ -0,0 +1,15 @@
+package tpfinalpoo2;
+
+public class FacturaShipper extends Factura{
+
+	public FacturaShipper(Orden orden) {
+		super(orden);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Double costoTotal() {
+		return null;
+	}
+
+}
